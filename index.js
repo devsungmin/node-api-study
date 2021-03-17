eed8343c7573482e4ac64ae3bd3d5fbd5fe782cb
devsungmin <kimsungmin.dev@gmail.com>
2021-03-17 20:33:13 +0900

    Fix: mysql db integration

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const app = express();
 const routes = require('./routes');
+const sequelize = require('./models').sequelize;
 
 app.use('/api', routes);
+
+sequelize.sync();
 
 app.listen(3000, (req, res) => {
     console.log("Server connection")
